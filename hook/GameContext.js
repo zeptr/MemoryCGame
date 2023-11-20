208bdb0bad3343b1265d354feec355ebe752866c
Jose <107567749+zeptr@users.noreply.github.com>
2023-11-21 01:43:16 +0200

    ✨ Added the Fisher-Yates Shuffle Algorithm

--- hook/GameContext.js
+++ hook/GameContext.js
@@ -9,11 +9,30 @@ export const GameProvider = ({ children }) => {
     const [matchedPairs, setMatchedPairs] = useState([]); 
 
     const initializeGame = () => {
-        
+        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+        const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+        let deck = [];
+
+        // Create a deck of cards
+        suits.forEach(suit => {
+            ranks.forEach(rank => {
+                deck.push({ suit, rank, isFlipped: false, isMatched: false });
+            });
+        });
+
+        // Fisher-Yates Shuffle Algorithm
+        for (let i = deck.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+
+        setCards(deck);
         setIsGameActive(true);
+        setFlippedCards([]);
+        setMatchedPairs([]);
     };
 
-    const flipCard = cardId => {
+    const flipCard = () => {
         
     };
 

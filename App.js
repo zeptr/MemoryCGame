45c7d4578476b821fbab8bf94b52621aa2e66f6b
Jose <107567749+zeptr@users.noreply.github.com>
2023-11-21 17:44:57 +0200

    ✨ Implemented the navigation functionality

--- App.js
+++ App.js
@@ -1,22 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { GameProvider } from './hook/GameContext';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <GameProvider>
-        <View style={styles.container}>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                
+            </Stack.Navigator>
+        </NavigationContainer>
     </GameProvider>
     
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
